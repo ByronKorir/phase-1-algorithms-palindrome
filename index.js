08b63471a815e71a60b67dc5a13c534b877a481c
byron korir <byron.korir@student.moringaschool.com>
2023-10-06 00:38:46 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
   word = word.toLowerCase();
-  //checking if word is a palindrome
+// Reverse the word and check if it is equal to the original word
  return word === word.split('').reverse().join('');
  }
 
 
 /* 
   Add your pseudocode here
-*///the function should take a String
-// convert the string to lowercase(using.toLowerCase())method
-// check if the reverse and forward matches using=> [.split().reverse().join()] methods
+*/
+//the isPalindrome takes a string argument
+//convert string to lowercase
+// Reverse the word and check if it is equal to the original word
 
 
 
 /*
   Add written explanation of your solution here
+
+  //the function should take a String
+// convert the string to lowercase(using.toLowerCase())method
+// check if the reverse and forward matches using=> [.split().reverse().join()] methods
 */
 
 // You can run `node index.js` to view these console logs
